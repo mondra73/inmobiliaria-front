5e7b69d5cc799d7ce109ac6008776c63bd3b1a2d
Javier <javier.espinosa@live.com.ar>
2025-07-16 11:37:30 -0300

    re direccion correcta al dominio real

--- src/main.js
+++ src/main.js
@@ -6,7 +6,15 @@ import './assets/tailwind.css'
 import vuetify from './plugins/vuetify'
 import './assets/fonts/fonts.css';
 
-const app = createApp(App)
-app.use(vuetify)
-app.use(router)
-app.mount('#app')
+// 🔁 Redirección desde onrender.com al dominio real
+if (
+  window.location.hostname === 'espinosainmob.onrender.com' ||
+  window.location.hostname === 'espinosainmob.com.ar'
+) {
+  window.location.href = `https://www.espinosainmob.com.ar${window.location.pathname}${window.location.search}`;
+} else {
+  const app = createApp(App)
+  app.use(vuetify)
+  app.use(router)
+  app.mount('#app')
+}
